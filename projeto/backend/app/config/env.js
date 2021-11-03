5222ac3b1c87129a8632e1dc464316711011a6ea
alexsanderk2 <80762201+alexsanderk2@users.noreply.github.com>
2021-11-03 16:24:16 -0300

    projeto final do curso de javascript

---
+++ projeto/backend/app/config/env.js
@@ -0,0 +1,9 @@
+const env = {
+    database: 'teste',
+    username: 'root',
+    password: '12345',
+    host: 'localhost',
+    dialect: 'mysql',
+  };
+  
+  module.exports = env;
---
+++ projeto/frontend/src/App.js
@@ -0,0 +1,14 @@
+import Routes from './routes';
+import GlobalStyle from './estilo';
+
+
+function App() {
+  return (
+    <>
+    <Routes/>  
+    <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
